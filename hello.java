2c9b5c934b2d3dc0e94dd5fe0c3ec49a6df1e268
kimdg <eorhks7@gmail.com>
2019-12-18 20:00:39 +0900

    hello java! second push

--- hello.java
+++ hello.java
@@ -5,7 +5,9 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello Java!");
-		
+		System.out.println("두번째 수정");
+		system.out.println("오픈소스 과제중!");
+
 
 	}
 
